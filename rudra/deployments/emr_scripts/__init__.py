7bf3b4fd02f3769628c542afddd0f9df5e9103ee
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-08-29 07:48:08 +0200

    Fix issue #117: Dead code has been detected in rudra/deployments/emr_scripts/__init__.py

--- rudra/deployments/emr_scripts/__init__.py
+++ rudra/deployments/emr_scripts/__init__.py
@@ -5,3 +5,8 @@ from rudra.deployments.emr_scripts.npm_emr import NpmEMR
 from rudra.deployments.emr_scripts.pypi_emr import PyPiEMR
 
 __all__ = ['MavenEMR', 'NpmEMR', 'PyPiEMR']
+
+# make all linters happy
+assert MavenEMR is not None
+assert NpmEMR is not None
+assert PyPiEMR is not None
